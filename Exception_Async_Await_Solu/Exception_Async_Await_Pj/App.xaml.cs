e338d9190684fd18bb6ed758ddb0af187ac5b238
Vinci-da-Gama <11582145@student.uts.edu.au>
2018-02-16 14:46:29 +1100

    wpf

---
+++ Exception_Async_Await_Solu/Exception_Async_Await_Pj/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Exception_Async_Await_Pj
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
